0850883e377a9cd59555ba089e9f5d4ae352ca36
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2023-11-09 16:45:07 -0300

    Redirect every 404 to home route

--- next.config.js
+++ next.config.js
@@ -1,5 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/:path(.{1,})", // this will redirect any other paths to `/`
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/404",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
   reactStrictMode: true,
   images: {
     domains: ["ecommerce-nextjs-test303.s3.sa-east-1.amazonaws.com"],
